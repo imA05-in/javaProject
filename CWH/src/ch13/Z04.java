e25d5195dbc81d36ebcdb5d46ef0bcd8091013f5
abhishek <abhishek29112005@gmail.com>
2024-12-24 15:41:32 +0530

    new file:   src/ch13/Z04.java

---
+++ CWH/src/ch13/Z04.java
@@ -0,0 +1,14 @@
+package ch13;
+
+public class Z04 {
+    public static void main(String[] args) {
+
+    }
+    int hi(){
+        int a = 5;
+        int b = 6;
+        return b;
+        int c = 6;
+        
+    }
+}
